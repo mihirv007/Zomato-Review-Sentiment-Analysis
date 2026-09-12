3d0ce2e95dfefc0b91802675be96761c78e30709
Mihir Verma <mihirverma000@gmail.com>
2026-09-12 15:49:51 +0530

    fix: replace raw template render with standard flask render_template

--- review_analysis_server.py
+++ review_analysis_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+=from flask import Flask, render_template, request
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 import joblib
